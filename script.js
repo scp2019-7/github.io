00bae0c252c3308ff068430afc83e1999893ab77
Haruto <ynhrt1250@gmail.com>
2019-11-27 12:12:59 +0900

    add image

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const $ = e => document.getElementById(e);
 $("button1").onclick = function() {
   const formdata = new FormData();
   const n = new XMLHttpRequest();
-  n.open("GET", "/sample.txt");
+  n.open("GET", "sample.txt");
   n.onload = function() {
     $("text1").textContent = this.responseText;
   };
   n.send(formdata);
+};
+
+$("button2").onclick = function() {
+  $("img1").src = "map.png";
 };
