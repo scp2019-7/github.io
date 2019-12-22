e4c1bba1b9b01c71cc3b9ce62b31fc4f414ca1e7
Haruto <ynhrt1250@gmail.com>
2019-12-22 17:54:43 +0900

    こっちに変更 https://github.com/schmich/instascan/

--- script.js
+++ script.js
@@ -39,25 +39,3 @@ $("button3").onclick = function() {
   var gID = "002";
   $("img1").src = getRootimage(sID, gID);
 };
-
-function openQRCamera(node) {
-  var reader = new FileReader();
-  reader.onload = function() {
-    node.value = "";
-    qrcode.callback = function(res) {
-      if (res instanceof Error) {
-        alert(
-          "No QR code found. Please make sure the QR code is within the camera's frame and try again."
-        );
-      } else {
-        node.parentNode.previousElementSibling.value = res;
-      }
-    };
-    qrcode.decode(reader.result);
-  };
-  reader.readAsDataURL(node.files[0]);
-}
-
-function showQRIntro() {
-  return confirm("Use your camera to take a picture of a QR code.");
-}
