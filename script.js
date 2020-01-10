11f06a19d0ca30fa5b3c77a548c7db887d792b84
kokiOnbe <kotitech@icloud.com>
2020-01-10 21:39:19 +0900

    aaaaa

--- script.js
+++ script.js
@@ -2,20 +2,15 @@
 function $(e) {
   return document.getElementById(e);
 }
-function getText(path, onload) {
-  const n = new XMLHttpRequest();
-  n.open("GET", path);
-  n.onload = onload;
-  n.send(null);
-}
-function getQRimage() {
+
+function getQRID() {
   var urlParam = location.search.substring(1);
   if (urlParam) {
     var param = urlParam.split("&");
     if (param.length == 1) {
-      var Iname = param[0] + ".png";
+      var QRID = param[0];
     }
-    return Iname;
+    return QRID;
   }
 }
 
@@ -24,18 +19,107 @@ function getRootimage(sID, gID) {
   return Iname;
 }
 
-$("button1").onclick = function() {
-  getText("./sample.txt", function() {
-    $("text1").textContent = this.responseText;
+function csvToArray(path) {
+        var csvData = new Array();
+        var data = new XMLHttpRequest();        
+        data.open("GET", path, false);
+        data.send(null);
+        var LF = String.fromCharCode(10);
+        var lines = data.responseText.split(LF);
+        for (var i = 0; i < lines.length;++i) {
+                var cells = lines[i].split(",");
+                if( cells.length != 1 ) {
+                        csvData.push(cells);
+                }
+        }
+        return csvData;
+}
+
+
+// sample root
+var rootlist = [
+  [100,120],
+  [80,120],
+  [80,160],
+  [190,160],
+  [190,200]
+];
+
+jQuery(function(){  
+  jQuery('#text1').autocomplete({
+    source: function(request, response){
+      var suggests = [];
+      var regexp = new RegExp('(' + request.term + ')');
+      
+      jQuery.each(dataList, function(i, values){
+        if(values[0].match(regexp) || values[1].match(regexp) || values[2].match(regexp)){
+          suggests.push(values[0]);
+        }
+      });
+      
+      response(suggests);
+    },
+    autoFocus: true,
+    delay: 300,
+    minLength: 1
   });
-};
+});　
 
-$("button2").onclick = function() {
-  $("img1").src = getQRimage();
-};
+function kensaku() {
+  document.getElementById("text2").innerText = document.getElementById("text1").value;
+}
 
-$("button3").onclick = function() {
-  var sID = "001";
-  var gID = "002";
-  $("img1").src = getRootimage(sID, gID);
+function hoge(code)
+{
+	//エンターキー押したら
+	if(13 === code)
+	{
+		kensaku();
+	}
+}
+
+
+window.onload = function() {
+  var canvas = document.getElementById('axisCanvas');
+  var canvasW = 200;
+  var canvasH = 300;
+  canvas.width = canvasW;
+  canvas.height = canvasH;
+  var image = new Image();
+  let imagePath = "database/HonkanMap_1F.svg";
+  image.src = imagePath;
+  var ctx = canvas.getContext('2d'); 
+  canvas.onclick = function(e) {
+    
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    
+    var rect = e.target.getBoundingClientRect();
+    var mouseX = e.clientX - Math.floor(rect.left) - 2;
+    var mouseY = e.clientY - Math.floor(rect.top) - 2;
+
+    // クリック位置を中心に円を描画
+    
+    ctx.beginPath();
+    ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight, 0, 0, canvasW, canvasH);
+    ctx.fillStyle = 'hsl( 0, 100%, 50% )';
+    ctx.arc(mouseX, mouseY, 5, 0, Math.PI * 2, false);
+    ctx.fill();
+     
+    // 座標の表示テキストを描画
+    var maxWidth = 100;
+    ctx.textAlign = 'right';
+    ctx.fillText('( ' + mouseX + ', ' + mouseY + ' )', canvasW - 20, canvasH - 20, maxWidth);
+    ctx.closePath();
+    
+    // 直線
+    ctx.beginPath();
+    ctx.strokeStyle = 'hsl( 0, 100%, 50% )';
+    ctx.lineWidth = 5;
+    ctx.moveTo(rootlist[0][0],rootlist[0][1]);
+    for(var i=1;i<rootlist.length;i++) {
+    ctx.lineTo(rootlist[i][0],rootlist[i][1]);
+    }
+    ctx.stroke();
+    ctx.closePath();
+  }
 };
